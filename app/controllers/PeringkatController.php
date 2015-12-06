116b9551335d652d1ef9bef022997fa25fad0024
chandra92kurniawan <chakz790@gmail.com>
2015-12-06 17:28:59 +0700

    asdc

--- app/controllers/PeringkatController.php
+++ app/controllers/PeringkatController.php
@@ -5,7 +5,15 @@ class PeringkatController extends \Phalcon\Mvc\Controller
 	public function indexAction(){
 		$this->view->title = "Outbox - ";
 		$this->view->selectmenu	= "outbox";
-		$user=Users::find();
+		$user=Users::find(
+				array(
+					array(
+						"sort"=>array(
+							"pungutan"=>1
+						),
+						"limit"=>5
+					)
+		));
 		foreach($user as $u){
 			echo $u->pungutan."<br>";
 		}
